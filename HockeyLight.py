0fe421e42c41d12a997b0d09badf464e2eed1ed4
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2022-12-06 22:04:43 -0800

    New Year

--- HockeyLight.py
+++ HockeyLight.py
@@ -1,95 +1,103 @@
 import datetime
 import time
-import subprocess
-import RPi.GPIO as GPIO
-import urllib.request
-import vlc
+# import RPi.GPIO as GPIO
+# import vlc
 import re
-from pygame import mixer
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
+# Run with cronjob at 8am Eastern
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+options.add_argument('--headless')
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", chrome_options=options)
+
+def get_todays_game(team):
+    return "Now"
+    try:
+        driver.get("https://www.nhl.com/scores")
+        date_button = driver.find_element(By.CLASS_NAME, "datepicker__button")
+        driver.execute_script("arguments[0].click();", date_button)
+        today_button = driver.find_element(By.CLASS_NAME, "today")
+        driver.execute_script("arguments[0].click();", today_button)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        game_times = soup.find_all('p', {"class": "g5-component--nhl-scores__status-game-time"})
+        team_names = soup.find_all('span', {"class": "g5-component--nhl-scores__team-name"})
+        for i in range(len(team_names)):
+            if team_names[i].get_text() == team:
+                return game_times[i//2].get_text()
+        return None
+    except:
+        return None
 
 def get_score(team):
-    """
-    Return the score of the selected team.
-
-    Requires the Beautifulsoup4 library and Rpi.GPIO module.
-
-    team = "team three letter abbreviation"
-    The script is configured to work with www.nhl.com/ice/m_scores.htm
-    and will need to be modified if other URLs are used.
-    If the given team is not on the schedule to play 998 will be returned.
-    """
-
-    url = "http://www.nhl.com/ice/m_scores.htm"
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, "html.parser")
-    team_list = soup.find('a', href=re.compile(team))
     try:
-        for td in team_list.parent.find_next_siblings('td'):
-            current_score = td.text 
-            return int(current_score)
+        driver.get("https://www.nhl.com/scores")
+        date_button = driver.find_element(By.CLASS_NAME, "datepicker__button")
+        driver.execute_script("arguments[0].click();", date_button)
+        today_button = driver.find_element(By.CLASS_NAME, "today")
+        driver.execute_script("arguments[0].click();", today_button)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'lxml')
+        if soup.find('span', text=re.compile(team)).parent.parent.parent.parent.parent.find('span', text=re.compile('Final')):
+            return 998
+        return int(soup.find('span', text=re.compile(team)).parent.parent.find_next_sibling().get_text())
     except:
-        current_score = 998
-        return int(current_score)
+        return 999
 
 def main():
-    """
-    Alert when a goal is scored by a specified team.
-
-    Requires vlc on Raspberry Pi.
-
-    If get_score() returns 0 there has not been a goal in the game so it is
-    safe to reset old_score to 0.
-    If get_score() returns 998 then the desired team is not on the schedule
-    and the loop will wait one hour before restarting.
-    If get_score() returns a reasonable score greater than old_score it will
-    indicate a goal has been scored, triggering the sound and light and
-    setting old_score equal to new_score.
-    """
-
-    # Set first old_score variable
-    old_score = 0
-    # Set first current hour and month variable
+    team = 'Sharks' # 'Blue Jackets' # "Panthers" # "Blackhawks" # 'Penguins'
+    game_time = get_todays_game(team)
     now = datetime.datetime.now()
-    hour = int(now.strftime("%H"))
-    #month = now.month
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)  
-    GPIO.setup(2, GPIO.OUT)  # Setup GPIO Pin to 2
+    tomorrow = now.replace(hour=0, minute=0, second=0) + datetime.timedelta(days=1)
+    month = now.month
+    if game_time and (month < 6 or month > 9):
+        # h = int(game_time[0 : game_time.index(":")]) - 3
+        # m = int(game_time[game_time.index(":")+1 : game_time.index(":")+3])
+        # if game_time.find("PM"):
+        #     h = h + 12
+        # game_time = now.replace(hour=h, minute=m)
+        # time.sleep((game_time-now).total_seconds())
 
-    continuous = True  # Loop to run 24/7/365
-    while continuous:
-        """while month in (6, 7, 8, 9):
-            time.sleep(86400)
-            now = datetime.datetime.now()
-            month = now.month"""
-        while hour < 13:
-            time.sleep(120)
-            now = datetime.datetime.now()
-            hour = int(now.strftime("%H"))
-        while hour >= 13:
-            try: 
-                new_score = get_score("SJS")
-            except:
-                new_score = 999;
-            #new_score = 1
+        # GPIO.setwarnings(False)
+        # GPIO.setmode(GPIO.BCM)  
+        # GPIO.setup(2, GPIO.OUT)  # Setup GPIO Pin to 2
+        old_score = 0
+        errors = 0
+        on_power_play = False
+        now = datetime.datetime.now()
+        while now < tomorrow:
+            new_score = get_score(team)
+            # {new_score, power_play} = get_score(team)
             print(new_score)
-            if new_score == 0:
-                old_score = new_score
-            elif new_score >= 998:  # Team not on schedule wait 24 hours 
-                time.sleep(86400)
+            if new_score >= 999:  # Error occurred
+                errors += 1
+                if errors >= 5:
+                    return 1
+            elif new_score == 998:
+                print('FINAL')
+                return 0
             elif new_score > old_score:  # Goal has been scored
-                print('GOAL!:' + str(new_score))
-                GPIO.output(2, True)  # Light ON
-                p= vlc.MediaPlayer("SJGoalHorn.mp3")
-                p.play() # Play sound
+                print('GOAL!')
+                # GPIO.output(2, True)  # Light ON
+                p = vlc.MediaPlayer("SJGoalHorn.mp3")
+                p.play() # Play Goal Song
                 time.sleep(36)
-                GPIO.output(2, False)     # Light OFF
-                old_score = new_score 
-            now = datetime.datetime.now()  # Update hour
-            hour = int(now.strftime("%H"))
-            time.sleep(2)  
+                # GPIO.output(2, False)     # Light OFF
+                old_score = new_score
+
+            # if power_play and not on_power_play:
+            #     print("POWER PLAY!")
+            #     #p = vlc.MediaPlayer("SJPowerPlay.mp3")
+            #     # p.play() # Play power play Song
+            #     on_power_play = True
+            # elif not power_play and on_power_play:
+            #     on_power_play = False
+            
+            now = datetime.datetime.now()
 
 if __name__ == "__main__":
     main()
